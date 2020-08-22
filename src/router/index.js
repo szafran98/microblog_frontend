3c46eaf0e82ba12e5b6f43ef6b3d4a95456db243
szafran98 <szafran1308@gmail.com>
2020-08-22 19:51:35 +0200

    Added working user activation component, modified routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from "../views/Login";
+import UserActivation from "../views/UserActivation";
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ Vue.use(VueRouter)
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/activate/:uid/:token',
+      name: 'UserActivation',
+      component: UserActivation
     }
 ]
 
--- src/views/UserActivation.vue
+++ src/views/UserActivation.vue
@@ -1,10 +1,22 @@
 <template>
-    
+    <div>
+        <h1 style="color: white">Account activated</h1>
+    </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        name: "UserActivation"
+        name: "UserActivation",
+        created() {
+            let data = this.$route.params
+            axios.post('http://127.0.0.1:8000/auth/users/activation/', data)
+                .then( () => {
+                    setTimeout(() => {
+                        this.$router.push({ name: 'Home' })
+                    }, 3000)
+                })
+        }
     }
 </script>
 
