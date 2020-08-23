d63261f7f02df28a6b94cbcce8e540be026e865a
szafran98 <szafran1308@gmail.com>
2020-08-23 12:59:14 +0200

    Added scss stylesheets and scss config

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,24 @@ module.exports = {
     output: {
       libraryExport: 'default'
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+            'vue-style-loader',
+            'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              prependData: `
+              @import "@/assets/scss/variables.scss";
+              @import "@/assets/scss/mixins.scss";`
+            }
+          }
+        ]
+      }
+    ]
   }
 }
